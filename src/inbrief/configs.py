dc514fc25227433f3989db05e0ce7f0a2d243f56
limjiyoon <luinpark@gmail.com>
2025-01-04 13:33:02 +0900

    FEATURE: read config from toml

---
+++ src/inbrief/configs.py
@@ -0,0 +1,33 @@
+"""Configuration settings for the application."""
+
+from pydantic import Field
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    TomlConfigSettingsSource,
+)
+
+from src.inbrief.utils import root_dir
+
+
+class Config(BaseSettings):
+    """"""
+
+    youtube_api_key: str = Field(...)
+    model_config = SettingsConfigDict(toml_file=root_dir / "config.toml")
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        _ = init_settings, env_settings, dotenv_settings, file_secret_settings  # required, but don't use
+        return (TomlConfigSettingsSource(settings_cls),)
+
+
+CONFIG = Config()  # pyright: ignore[reportCallIssue]
