96b1fe9fbfaff9e7329b314646c9d76c754c3fd3
limjiyoon <luinpark@gmail.com>
2025-01-04 15:35:50 +0900

    FEATURE: fetch transcript

--- src/inbrief/youtube_inbrief.py
+++ src/inbrief/youtube_inbrief.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+from youtube_transcript_api import YouTubeTranscriptApi
+
 
 class YoutubeInBrief:
     """Download transcript from YouTube video and summarize it.
 
     Usage:
     >>> yt = YoutubeInBrief()
-    >>> transcript = yt.fetch_transcript(video_url)
+    >>> transcript = yt.fetch_transcript(video_id)
     >>> summary = yt.summarize(transcript)
     """
 
@@ -14,11 +16,24 @@ class YoutubeInBrief:
         pass
 
     # TODO: define more elaborate type of transcript
-    def fetch_transcript(self, video_id: str) -> None:
-        """Fetch transcript from YouTube video."""
-        pass
+    def fetch_transcript(self, video_id: str) -> list:
+        """Fetch transcript from YouTube video.
+
+        If there is english transcript available, it will be fetched.
+        If not and there is korean transcript available, it will be fetched.
+        Otherwise, it will return None.
+        """
+        return YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=["en", "ko"])
 
     # TODO: define more elaborate type of transcript and return type
     def summarize(self, transcript: list) -> None:
         """Summarize the transcript."""
         pass
+
+
+if __name__ == '__main__':
+    # Yannic Kilcher's video on "Byte Latent Transformer: Patches Scale Better Than Tokens (Paper Explained)"
+    video_id = "loaTGpqfctI"
+    youtube_inbrief = YoutubeInBrief()
+    transcript = youtube_inbrief.fetch_transcript(video_id)
+    print(transcript)
