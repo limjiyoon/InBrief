23f82864ef951a605d950c4328227a16dcac8dcc
limjiyoon <luinpark@gmail.com>
2025-01-04 23:16:04 +0900

    FEATURE: implement summarize method

--- src/inbrief/summarizer.py
+++ src/inbrief/summarizer.py
@@ -11,13 +11,3 @@ class Summarizer:
 
     def summarize(self, text: str) -> str:
         return self._model.generate_content(["Summarize following contents", text]).text
-
-
-if __name__ == "__main__":
-    yt = YoutubeInBrief()
-    video_url = "https://www.youtube.com/watch?v=loaTGpqfctI"
-    transcript = yt.fetch_transcript(video_url)
-    text = " ".join(chunk.text for chunk in transcript.chunks)
-    summarizer = Summarizer("models/gemini-1.5-flash")
-    summary = summarizer.summarize(text)
-    print(summary)
--- src/inbrief/youtube_inbrief.py
+++ src/inbrief/youtube_inbrief.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
+from inbrief.summarizer import Summarizer
 from inbrief.types import Transcript
 from inbrief.utils import video_id_from_youtube_url, wrap_transcript
 
@@ -19,6 +20,9 @@ class YoutubeInBrief:
      # Note: summarize() is not yet implemented
     """
 
+    def __init__(self, summarizer: Summarizer):
+        self._summarizer = summarizer
+
     def fetch_transcript(self, video_url: str) -> Transcript:
         """Fetch transcript from YouTube video.
 
@@ -32,17 +36,16 @@ class YoutubeInBrief:
             raise ValueError(f"Transcript not found in the provided URL: {video_url}.")
         return wrap_transcript(raw_transcript)
 
-    # TODO: define more elaborate type of transcript and return type
-    @staticmethod
-    def summarize(transcript: list) -> None:
+    def summarize(self, transcript: Transcript) -> str:
         """Summarize the transcript."""
-        # TODO: implement summarization algorithm
-        pass
+        text = " ".join(chunk.text for chunk in transcript.chunks)
+        return self._summarizer.summarize(text)
 
 
 if __name__ == "__main__":
     # Yannic Kilcher's video on "Byte Latent Transformer: Patches Scale Better Than Tokens (Paper Explained)"
     video_url = "https://www.youtube.com/watch?v=loaTGpqfctI"
-    youtube_inbrief = YoutubeInBrief()
+    summarizer = Summarizer("models/gemini-1.5-flash")
+    youtube_inbrief = YoutubeInBrief(summarizer=summarizer)
     transcript = youtube_inbrief.fetch_transcript(video_url)
     print(transcript)
