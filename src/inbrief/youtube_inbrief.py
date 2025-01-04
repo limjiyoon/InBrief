13f0db31cab5999edb9e927de87a5268e09dd627
limjiyoon <luinpark@gmail.com>
2025-01-04 16:41:48 +0900

    ENHANCE: fetch transcript from url

--- src/inbrief/youtube_inbrief.py
+++ src/inbrief/youtube_inbrief.py
@@ -9,26 +9,33 @@ class YoutubeInBrief:
     """Download transcript from YouTube video and summarize it.
 
     Usage:
+    >>> video_url = "[YouTube video URL]"
     >>> yt = YoutubeInBrief()
-    >>> transcript = yt.fetch_transcript(video_id)
+    >>> transcript = yt.fetch_transcript(video_url)
     >>> summary = yt.summarize(transcript)
     """
 
-    def __init__(self):
-        pass
-
     # TODO: define more elaborate type of transcript
-    def fetch_transcript(self, video_id: str) -> Transcript:
+    def fetch_transcript(self, video_url: str) -> Transcript:
         """Fetch transcript from YouTube video.
 
-        If there is english transcript available, it will be fetched.
-        If not and there is korean transcript available, it will be fetched.
-        Otherwise, it will return None.
+        If an English transcript is available, it will be fetched.
+        If not, a Korean transcript will be fetched if available.
+        Otherwise, a ValueError will be raised.
         """
+        video_id = self._video_id_from_url(video_url)
         raw_transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=["en", "ko"])
         if raw_transcript is None:
             raise ValueError("Transcript not found.")
         return self._wrap_transcript(raw_transcript)
+
+    def _video_id_from_url(self, url: str) -> str:
+        """Extract video ID from YouTube video URL.
+
+        Example:
+            "https://www.youtube.com/watch?v=loaTGpqfctI" -> "loaTGpqfctI"
+        """
+        return url.split("v=")[1]
 
     def _wrap_transcript(self, raw_transcript: list) -> Transcript:
         """Wrap raw transcript into a Transcript object."""
@@ -40,6 +47,7 @@ class YoutubeInBrief:
         )
 
     # TODO: define more elaborate type of transcript and return type
+    # TODO: implement summarization algorithm
     def summarize(self, transcript: list) -> None:
         """Summarize the transcript."""
         pass
@@ -47,7 +55,7 @@ class YoutubeInBrief:
 
 if __name__ == "__main__":
     # Yannic Kilcher's video on "Byte Latent Transformer: Patches Scale Better Than Tokens (Paper Explained)"
-    video_id = "loaTGpqfctI"
+    video_url = "https://www.youtube.com/watch?v=loaTGpqfctI"
     youtube_inbrief = YoutubeInBrief()
-    transcript = youtube_inbrief.fetch_transcript(video_id)
+    transcript = youtube_inbrief.fetch_transcript(video_url)
     print(transcript)
