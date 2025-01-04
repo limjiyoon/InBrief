845791052585a0e2b46d937fefd7a6b49690ecdd
limjiyoon <luinpark@gmail.com>
2025-01-04 23:34:42 +0900

    ENHANCE: use raise when checking whether model is available

--- src/inbrief/summarizer/simple_summarizer.py
+++ src/inbrief/summarizer/simple_summarizer.py
@@ -6,7 +6,8 @@ class SimpleSummarizer(Summarizer):
     """Summarize text using a language model with simple script."""
 
     def __init__(self, model_name: str):
-        assert model_name in LlmFactory().available_models(), f"Model '{model_name}' is not available."
+        if model_name not in LlmFactory().available_models():
+            raise ValueError(f"Model '{model_name}' is not available.")
         self._model = LlmFactory().create(model_name)
 
     def summarize(self, text: str) -> str:
