000ff6e3da1169db339d91aab3559c85546b1a2b
limjiyoon <luinpark@gmail.com>
2025-01-10 22:29:58 +0900

    FEATURE: add basic graph view

---
+++ src/frontend/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import '@xyflow/svelte/dist/style.css';
+  import { SvelteFlow, SvelteFlowProvider } from '@xyflow/svelte';
+  import GraphView from './GraphView.svelte';
+</script>
+
+<main>
+<!-- <SvelteFlowProvider> -->
+<GraphView />
+<!-- </SvelteFlowProvider> -->
+  
+</main>
---
+++ src/frontend/src/GraphView.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { writable } from 'svelte/store';
+    import { SvelteFlow } from '@xyflow/svelte';
+   
+    const nodes = writable([
+      { id: 'a', data: { label: 'node a' }, position: { x: 0, y: 0 } },
+      { id: 'b', data: { label: 'node b' }, position: { x: 0, y: 100 } }
+    ]);
+   
+    const edges = writable([
+      { id: 'e1-2', source: 'a', target: 'b' }
+    ]);
+</script>
+   
+<div style="height:100vh;">
+    <SvelteFlow {nodes} {edges} fitView class="bg-teal-50">
+    </SvelteFlow>
+</div>
