f64ce942619d5f5537396e9f6e71ae47467ff57d
limjiyoon <luinpark@gmail.com>
2025-01-11 20:16:25 +0900

    FEATURE: add cors middleware

--- src/inbrief/main.py
+++ src/inbrief/main.py
@@ -3,8 +3,22 @@ from fastapi.responses import ORJSONResponse
 import uvicorn
 
 from inbrief.controllers.youtube_controller import router as YoutubeRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(YoutubeRouter)
 
 
